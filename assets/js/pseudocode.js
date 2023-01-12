24062e0e77da8fb42dd6b1ed552fa4e10a759c5d
pandi1813 <andrea.peter1813@gmail.com>
2023-01-12 22:01:08 +0000

    created pseudocode

---
+++ assets/js/pseudocode.js
@@ -0,0 +1,41 @@
+// need questions
+    // can use array of objects and pass values to DOM 
+    // let questions = [
+    //     question1 = {
+    //         question: "value"
+    //         answer: "value"
+    //         answer: "value"
+    //         answer: "value"
+    //     },
+    // question2 = {
+    //         question: "value"
+    //         answer: "value"
+    //         answer: "value"
+    //         answer: "value"
+    //     }
+    // ]
+
+
+    // buttons for each answer
+        // .createElement("")
+        // .textContent(object.value)
+        // .append
+        //when clicked go to next question (eventListener)
+            //if wrong answer subtract time from timer
+            // if correct answer add point to score
+                // variable to track score
+
+    // only display one question at a time
+            // for loop and move to next element when button clicked?
+
+//timer 
+    // change value of #time
+
+    //set timer
+    // https://developer.mozilla.org/en-US/docs/Web/API/setTimeout
+
+    // stops at 0 and gives shows final score and can enter name
+        // input for name -> save to localStorage
+    // retrieve score from localStorage
+
+//all questions answered -> end quiz, display high score + input for player name
