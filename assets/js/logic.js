334ba3e021aeb1254826535cdfca2714134b0119
pandi1813 <andrea.peter1813@gmail.com>
2023-01-13 16:47:32 +0000

    rearranged questions array structure and created variables that hook into index.html

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,17 @@
-console.log(questions)
+// variables to link into index.html
+
+let timerCount = document.querySelector("#time");
+let startScreen = document.querySelector("start-screen");
+let startButton = document.querySelector("#start");
+
+let questionsDiv = document.querySelector("questions");
+let questionTitle = document.querySelector("#question-title");
+let choices = document.querySelector("#choices");
+
+let endScreen = document.querySelector("#end-screen");
+let finalScore = document.querySelector("#final-score");
+let nameInput = document.querySelector("#initials");
+let submitButton = document.querySelector("#submit");
+
+let feedback = document.querySelector("#feedback");
+
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,13 +1,12 @@
 let questions = [
-    {question: "When did dinosaurs first appear?", answer1: "168 million years ago", answer2: "216 million years ago", correct: "252 million years ago", answer4: "314 million years ago"},
-    {question: "How many species of dinosaurs have been found in Britain?", answer1: "32", answer2: "54", answer3:"75", answer4: "108"},
-    {question: "Which was the dumbest dinosaur?", correct: "Stegosaurus", answer2: "Brachiosaurus", answer3:"Triceratops", answer4: "Edmontosaurus"},
-    {question: "Which was the largest carnivorous dinosaur?", answer1: "Tyrannosaurus Rex", correct: "Spinosaurus", answer3:"Gigantosaurus", answer4: "Indominus Rex"},
+ 
     {question: "Which was the heaviest dinosaur weighing as much as 17 african elephants?", answer1: "Brachiosaurus", answer2: "Notocolossus", answer3:"Megalodon", correct: "Argentinosaurus"},
-    {question: "", answer1: "", answer2: "", answer3:"", answer4: ""},
-    {question: "", answer1: "", answer2: "", answer3:"", answer4: ""},
-    {question: "", answer1: "", answer2: "", answer3:"", answer4: ""},
-    {question: "", answer1: "", answer2: "", answer3:"", answer4: ""},
-    {question: "", answer1: "", answer2: "", answer3:"", answer4: ""},
+ 
+    {question: "When did dinosaurs first appear?", answers: {answer1:"168 million years ago", answer2: "216 million years ago", answer3: "252 million years ago", answer4: "314 million years ago"}, correct: 2},
+    {question: "How many species of dinosaurs have been found in Britain?", answers: {answer1:"32", answer2: "54", answer3: "75", answer4: "108"}, correct: 3},
+    {question: "Which was the dumbest dinosaur?", answers: {answer1:"Stegosaurus", answer2: "Brachiosaurus", answer3: "Triceratops", answer4: "Edmontosaurus"}, correct: 0},
+    {question: "Which was the largest carnivorous dinosaur?", answers: {answer1:"Tyrannosaurus Rex", answer2: "Spinosaurus", answer3: "Gigantosaurus", answer4: "Indominus Rex"}, correct: 1},
+    {question: "Which was the heaviest dinosaur weighing as much as 17 african elephants?", answers: {answer1:"Brachiosaurus", answer2: "Notocolossus", answer3: "Megalodon", answer4: "Argentinosaurus"}, correct: 3},
+    {question: "", answers: {answer1:"", answer2: "", answer3: "", answer4: ""}, correct: 0},
 
 ]
